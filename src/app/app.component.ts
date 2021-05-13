553b5f778f560d2a8172a7d149eebc2a5d4b5428
Mithilesh shah <58083945+Mithilesh85@users.noreply.github.com>
2021-05-13 15:26:15 +0530

    this is emailGUI app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mithu',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/service/email.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+
+  private baseUrl:string="http://localhost:9090"
+
+  constructor(private http:HttpClient) { }
+
+  sendEmail(data:any){
+
+    return this.http.post(`${this.baseUrl}/sendemail`,data, {  responseType: 'text' })
+
+  }
+}
+ 
